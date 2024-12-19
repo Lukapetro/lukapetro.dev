919271222a2000da01ea6a487310e7b1b25b8014
Lukapetro <lucapetro93@gmail.com>
2024-12-19 10:04:14 +0100

    feat: project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+  integrations: [react(), tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    }
+  }
+})
